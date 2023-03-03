d77cc983c7fa1f76d666f831912d28c46dabde66
Mark Watson <markw@markwatson.com>
2023-03-03 09:21:31 -0700

    -

--- kg_search/Google_KG_helper.py
+++ kg_search/Google_KG_helper.py
@@ -22,5 +22,26 @@ def get_entity_info(entity_name):
     response = json.loads(urlopen(url).read())
     return response
 
+def tree_traverse(a_dict):
+    ret = []
+    def recur(dict_2, a_list):
+        if isinstance(dict_2, dict):
+            for key, value in dict_2.items():
+                if key in ['name', 'description', 'articleBody']:
+                    a_list += [value]
+                recur(value, a_list)
+        if isinstance(dict_2, list):
+            for x in dict_2:
+                recur(x, a_list)
+    recur(a_dict, ret)
+    return ret
+
+
+def get_context_text(entity_name):
+    json_data = get_entity_info(entity_name)
+    pprint(json_data)
+    pprint(tree_traverse(json_data))
+
 if __name__ == "__main__":
-    pprint(get_entity_info("Mark Louis Watson"))
+    #pprint(get_entity_info("Mark Louis Watson"))
+    get_context_text("Bill Clinton")
