23218baceab3ef2429e32be566af22fb652e8e2e
Mark Watson <markw@markwatson.com>
2023-03-10 13:56:20 -0700

    -

---
+++ rdf_query/dbpedia_rdf_query.py
@@ -0,0 +1,11 @@
+"Example from documentation"
+
+from llama_index import GPTSimpleVectorIndex, download_loader
+
+RDFReader = download_loader("RDFReader")
+doc = RDFReader().load_data("sample.nt")
+index = GPTSimpleVectorIndex(doc)
+
+result = index.query("list all countries in a quoted Python array, then explain why")
+
+print(result.response)
