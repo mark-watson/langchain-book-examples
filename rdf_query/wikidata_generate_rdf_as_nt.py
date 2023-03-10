8f29a88739f32c567110d232c289f2d6fee0a1fe
Mark Watson <markw@markwatson.com>
2023-03-10 15:05:44 -0700

    -

--- rdf_query/wikidata_generate_rdf_as_nt.py
+++ rdf_query/wikidata_generate_rdf_as_nt.py
@@ -1,24 +1,30 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 from rdflib import Graph
+import pandas as pd
 
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 sparql.setQuery("""
-    SELECT * WHERE { wd:Q80041 ?p ?o } limit 5
+   SELECT ?description ?is_a_type_of WHERE {
+      wd:Q80041 schema:description ?description FILTER (lang(?description) = 'en') .
+      wd:Q80041 wdt:P31 ?instanceOf .  
+      ?instanceOf rdfs:label ?is_a_type_of FILTER (lang(?is_a_type_of) = 'en') .
+   } limit 50
 """)
 
 sparql.setReturnFormat(JSON)
-results = sparql.queryAndConvert()
+results = sparql.query().convert()
 
 print("results:", results)
 
-g = Graph()
-g.parse(data=results, format="JSON")
+results_df = pd.json_normalize(results['results']['bindings'])
+print("\results_df:\n", results_df)
+r = results_df[['description.value', 'is_a_type_of.value']].head()
 
-print("\nresults:\n")
+print("\nresults:\n", r)
 # results = g.serialize(format="nt").encode("utf-8").decode('utf-8')
-print(results)
+#print(results)
 
 text_file = open("sample.nt", "w")
-text_file.write(results)
+text_file.write(r.to_string())
 text_file.close()
 
