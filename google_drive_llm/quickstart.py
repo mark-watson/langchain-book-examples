459e3fbe855e66affa77fe1338e6615759edca68
Mark Watson <markw@markwatson.com>
2023-03-12 13:13:37 -0700

    -

---
+++ google_drive_llm/quickstart.py
@@ -0,0 +1,5 @@
+from pydrive.auth import GoogleAuth
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
---
+++ google_drive_llm/test1.py
@@ -0,0 +1,20 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from pathlib import Path
+
+# Authenticate with Google
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+file1.SetContentString('Hello World!') # Set content of the file from given string.
+file1.Upload()
+
+# Auto-iterate through all files that matches this query
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+for file1 in file_list:
+  print('title: %s, id: %s' % (file1['title'], file1['id']))
+
