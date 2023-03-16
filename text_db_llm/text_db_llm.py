0f022270ee8ec351bb097e4894c037691f36d50f
Mark Watson <markw@markwatson.com>
2023-03-16 12:07:22 -0700

    -

--- text_db_llm/text_db_llm.py
+++ text_db_llm/text_db_llm.py
@@ -1,16 +1,19 @@
 # Text Database Supporting Search and Chat-based Exploration
 
-# make sure you set the following environment variable is set:
+# make sure you set the following environment variable:
 #   OPENAI_API_KEY
 
+import os
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
-documents = SimpleDirectoryReader('data').load_data()
-index = GPTSimpleVectorIndex(documents)
 
-# save to disk
-index.save_to_disk('index.json')
-# load from disk
-index = GPTSimpleVectorIndex.load_from_disk('index.json')
+if os.path.exists("index.json") and os.path.getsize("index.json") > 0:
+  print("Loading index from disk...")
+  index = GPTSimpleVectorIndex.load_from_disk('index.json')
+else:
+  print("Index file odes not exist, so create it...")
+  documents = SimpleDirectoryReader('data').load_data()
+  index = GPTSimpleVectorIndex(documents)
+  index.save_to_disk('index.json')
 
 # search for a document
 print(index.query("effect of body chemistry on exercise?"))
