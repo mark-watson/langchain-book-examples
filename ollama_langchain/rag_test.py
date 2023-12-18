6e56ad83cf0ee46179c0681480e3acdf5953caf1
Mark Watson <markw@markwatson.com>
2023-12-18 09:55:09 -0700

    adding examples for new book chapter Running Local LLMs Using Ollama

--- ollama_langchain/rag_test.py
+++ ollama_langchain/rag_test.py
@@ -4,26 +4,25 @@ from langchain.llms import Ollama
 from langchain.embeddings.ollama import OllamaEmbeddings
 from langchain.chains import RetrievalQA
 
-
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders.directory import DirectoryLoader
 
-
 # Create index (can be reused):
 
 loader = DirectoryLoader('../data', glob='**/*.txt')
-
 data = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1500, chunk_overlap=100)
+    chunk_size=100, chunk_overlap=100)
 all_splits = text_splitter.split_documents(data)
 
 persist_directory = 'cache'
 
 vectorstore = Chroma.from_documents(
-    documents=all_splits, embedding=OllamaEmbeddings(model="mistral:instruct"),persist_directory=persist_directory)
+    documents = all_splits,
+    embedding = OllamaEmbeddings(model="mistral:instruct"),
+    persist_directory=persist_directory)
 
 vectorstore.persist()
 
@@ -35,18 +34,18 @@ vectorstore = Chroma(persist_directory=persist_directory,
                      embedding_function=OllamaEmbeddings(model="mistral:instruct")
                     )
 
-llm = Ollama(base_url="http://localhost:11434",
-             model="mistral:instruct",
-             verbose=False,
+llm = Ollama(base_url = "http://localhost:11434",
+             model = "mistral:instruct",
+             verbose = False,
             )
 
 retriever = vectorstore.as_retriever()
 
 qa_chain = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type='stuff',
-            retriever=retriever,
-            verbose=True,)
+            llm = llm,
+            chain_type = 'stuff',
+            retriever = retriever,
+            verbose = True,)
 
 while True:
     query = input("Ask a question: ")
--- ollama_langchain/test.py
+++ ollama_langchain/test.py
@@ -1,13 +1,10 @@
 # requires "ollama serve" to be running in another terminal
 
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.llms import Ollama
 
 llm = Ollama(
     model="mistral:7b-instruct",
     verbose=False,
-    #callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
 )
 
 s = llm("how much is 1 + 2?")
