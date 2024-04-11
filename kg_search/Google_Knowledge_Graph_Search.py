538f51b6b05e956c49fbf8b6bc2b75bb80ea2b8f
Mark Watson <markw@markwatson.com>
2024-04-11 14:17:31 -0700

    fixed Google_Knowledge_Graph_Search.py example to use new versions of llama-index

--- kg_search/Google_Knowledge_Graph_Search.py
+++ kg_search/Google_Knowledge_Graph_Search.py
@@ -1,18 +1,15 @@
 """Example of Python client calling Knowledge Graph Search API."""
 
-from llama_index import GPTListIndex, Document
-
+from llama_index.core.schema import Document
+from llama_index.core import VectorStoreIndex
 import Google_KG_helper
 
 def kg_search(entity_name, *questions):
     ret = ""
     context_text = Google_KG_helper.get_context_text(entity_name)
     print(f"Context text: {context_text}")
-    doc = Document(context_text)
-    # llama_index < 0.5:
-    #index = GPTListIndex([doc])
-    # llama_index >= 0.5:
-    index = GPTListIndex.from_documents([doc])
+    doc = Document(text=context_text)
+    index = VectorStoreIndex.from_documents([doc])
     for question in questions:
         response = index.as_query_engine().query(question)
         ret += f"QUESTION:  {question}\nRESPONSE: {response}\n"
