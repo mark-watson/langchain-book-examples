31e1620a6259cef66e7f7ef73220e258a4777017
Mark Watson <markw@markwatson.com>
2024-05-04 08:43:57 -0700

    added a simple summarization example

---
+++ summarization/summarization_example.py
@@ -0,0 +1,28 @@
+import openai
+from openai import OpenAI
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
+
+# Read the prompt from a text file
+with open('prompt.txt', 'r') as file:
+    prompt_template = file.read()
+
+# Substitute a string variable into the prompt
+with open('../data/economics.txt', 'r') as file:
+    input_text = file.read()
+prompt = prompt_template.replace("input_text", input_text)
+
+# Use the OpenAI completion API to generate a response with GPT-4
+completion = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        },
+    ],
+)
+
+print(completion.choices[0].message.content)
