5072a96f47a37464548a0533d18a7d6d5d3df103
Mark Watson <markw@markwatson.com>
2024-05-14 09:53:08 -0700

    new example using OpenAI model gpt-4o released May 13, 2024

--- from_langchain_docs/README.md
+++ from_langchain_docs/README.md
@@ -3,3 +3,7 @@ pip install -U langchain-community
 pip install langchainhub
 
 Note: I no longer use Serp so I have not checked the search example recently.
+
+# new example using OpenAI model gpt-4o released May 13, 2024
+
+    python gpt_4o_test.py
---
+++ from_langchain_docs/gpt_4o_test.py
@@ -0,0 +1,14 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gpt-4o")
+
+messages = [
+    SystemMessage(content="You're a helpful assistant"),
+    HumanMessage(content="What is the purpose of model regularization? Be concise."),
+]
+
+results = llm.invoke(messages)
+print(results.content)
+print("\n\n\n")
+print(results)
