c4251706bcefa8244802eb15e82ac7c12d24aa7b
Mark Watson <markw@markwatson.com>
2024-08-27 13:44:15 -0700

    updated in memory Chroma vectorstore and RAG query example

--- text_db_llm/text_db_llm.py
+++ text_db_llm/text_db_llm.py
@@ -4,15 +4,37 @@
 #   OPENAI_API_KEY
 
 import os
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+chroma_client = chromadb.EphemeralClient()
+chroma_collection = chroma_client.create_collection("temp")
+
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+documents = SimpleDirectoryReader('data').load_data()
+index = VectorStoreIndex.from_documents(
+    documents, storage_context=storage_context, embed_model=embed_model
+)
+query_engine = index.as_query_engine()
+print(query_engine.query("effect of body chemistry on exercise?"))
+
+exit(0)
 
 if os.path.exists("index.json") and os.path.getsize("index.json") > 0:
   print("Loading index from disk...")
-  index = GPTSimpleVectorIndex.load_from_disk('index.json')
+  index = VectorStoreIndex.load_from_disk('index.json')
 else:
   print("Index file odes not exist, so create it...")
   documents = SimpleDirectoryReader('data').load_data()
-  index = GPTSimpleVectorIndex(documents)
+  index = VectorStoreIndex(documents)
+  print(dir(index))
   index.save_to_disk('index.json')
 
 # search for a document
